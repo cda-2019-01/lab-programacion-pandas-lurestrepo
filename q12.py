ea4cda193250b56492cf4c03ede97eeb7c803ad5
Lucas Restrepo <lukas0421@gmail.com>
2019-06-06 18:16:12 -0500

    scripts_pandas

--- q12.py
+++ q12.py
@@ -3,5 +3,10 @@
 ## y tbl2.tsv, compute la suma de tbl2._c5b por cada
 ## valor en tbl0._c1.
 ## 
-
+import pandas as pd
+import numpy as np
+x2 = pd.read_csv('tbl2.tsv', sep = '\t')
+## suma de tablas
+x2 = x2.groupby('_c5a')['_c5b'].sum()
+print(x2)
 
